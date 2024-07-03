00704bbc4fc9d3879470d437fd458e30e5300ceb
Vicky Bhattacharya <vickybhattacharya19@gmail.com>
2024-07-03 11:44:08 +0530

    updating the index.js giving server link

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ app.get("/login", (req, res)=>{
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Serving the app at http://localhost:${port}`)
 })
