c4b08f700289b37e391a9b59041a28b7cbca5f52
Vicky Bhattacharya <vickybhattacharya19@gmail.com>
2024-07-22 00:12:35 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -17,4 +17,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// Routes
+import userRouter from "./routes/user.routes.js";
+
+// routes declarations
+app.use("/api/v1/users", userRouter);
+
 export { app };
